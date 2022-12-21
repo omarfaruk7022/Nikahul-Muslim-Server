7cd539c751bbf69616abf37cd97c112dcb034d39
omarfaruk7022 <farhanfaruk221@gmail.comm>
2022-12-21 16:56:26 +0600

    test

--- index.js
+++ index.js
@@ -7,19 +7,76 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ewm5nbc.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ewm5nbc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri)
 
 async function run() {
   try {
     await client.connect();
     console.log("db connected");
+    const nameCollection = client.db("Nikahul-muslim").collection("name");
+    const simpleDataCollection = client
+      .db("Nikahul-muslim")
+      .collection("simple-data");
+    const addressCollection = client.db("Nikahul-muslim").collection("address");
+    const educationCollection = client
+      .db("Nikahul-muslim")
+      .collection("education");
+    const familyDetailsCollection = client
+      .db("Nikahul-muslim")
+      .collection("familyDetails");
+    const personalDocsCollection = client
+      .db("Nikahul-muslim")
+      .collection("personalDocs");
+    const aboutWeddingCollection = client
+      .db("Nikahul-muslim")
+      .collection("aboutWedding");
+    const etcDocsCollection = client.db("Nikahul-muslim").collection("etcDocs");
+
+    app.post("/addName", async (req, res) => {
+      const name = req.body;
+      const result = await nameCollection.insertOne(name);
+      res.json(result);
+    });
+    app.post("/simpleData", async (req, res) => {
+      const simpleData = req.body;
+      const result = await simpleDataCollection.insertOne(simpleData);
+      res.json(result);
+    });
+    app.post("/address", async (req, res) => {
+      const address = req.body;
+      const result = await addressCollection.insertOne(address);
+      res.json(result);
+    });
+    app.post("/education", async (req, res) => {
+      const education = req.body;
+      const result = await educationCollection.insertOne(education);
+      res.json(result);
+    });
+    app.post("/familyDetails", async (req, res) => {
+      const familyDetails = req.body;
+      const result = await familyDetailsCollection.insertOne(familyDetails);
+      res.json(result);
+    });
+    app.post("/personalDocs", async (req, res) => {
+      const personalDocs = req.body;
+      const result = await personalDocsCollection.insertOne(personalDocs);
+      res.json(result);
+    });
+    app.post("/aboutWedding", async (req, res) => {
+      const aboutWedding = req.body;
+      const result = await aboutWeddingCollection.insertOne(aboutWedding);
+      res.json(result);
+    });
+    app.post("/etcDocs", async (req, res) => {
+      const etcDocs = req.body;
+      const result = await etcDocsCollection.insertOne(etcDocs);
+      res.json(result);
+    });
   } finally {
   }
 }
